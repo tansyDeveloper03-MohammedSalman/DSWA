b6a6f6af56905a4a4e7acde61c1918252e333f3a
tansyDeveloper02 <dasari.arun@tansycloud.com>
2019-03-15 10:39:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const User = require("./routes/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const config = require("config");
+const app = express();
+
+config["jwtPrivateKey"] = "mySecureKey";
+
+if (!config.get("jwtPrivateKey")) {
+  // dswa_jwtPrivateKey = "mySecureKey";
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+} else {
+}
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb://usr_admin_read_write_devDSWA.clsdevdswa-shard-00-02-xn0wu.mongodb.net:27017,clsdevdswa-shard-00-01-xn0wu.mongodb.net:27017,clsdevdswa-shard-00-00-xn0wu.mongodb.net:27017/dbDevDSWA?ssl=true&replicaSet=clsDevDSWA-shard-0&authSource=admin&retryWrites=true",
+    {
+      auth: {
+        user: "usr_admin_read_write_devDSWA",
+        password: "shankerTansy01#"
+      }
+    }
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error(err));
+
+// mongoose
+//   .connect("mongodb://localhost/ArunDB")
+//   .then(() => console.log("Connected to MongoDB..."))
+//   .catch(err => console.error("Could not connect to MongoDB..."));
+// export dswa_jwtPrivateKey=mySecureKey
+
+app.use("/api/dswa", User);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/user.js
@@ -0,0 +1,133 @@
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const express = require("express");
+const router = express.Router();
+const { User, validate, validateLogin } = require("../models/user");
+const { auditLogin, auditLoginvalidate } = require("../models/audit_login");
+const ip = require("ip");
+const device = require("express-device");
+router.use(device.capture());
+
+router.post("/signup", async (req, res) => {
+  // const errors = validate(req.body);
+  const { errors, isValid } = validate(req.body);
+  if (!isValid) {
+    return res.status(400).send(errors);
+  }
+  console.log(errors);
+  let users = await User.findOne({ email: req.body.email });
+  if (users) return res.status(400).send("This email was used");
+
+  let user = new User(
+    _.pick(req.body, [
+      "first_name",
+      "last_name",
+      "email",
+      "password",
+      "country",
+      "city"
+    ])
+  );
+
+  // save the post data
+  user = await user.save();
+
+  // jwt
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send("success");
+});
+
+router.post("/login", async (req, res) => {
+  const { errors, isValid } = validateLogin(req.body);
+
+  if (!isValid) {
+    return res.status(400).send(errors);
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email");
+  // console.log(user);
+  const validPassword = req.body.password === user.password;
+
+  if (!validPassword) return res.status(400).send("Invalid email and password");
+  // audit_login insert
+  auditLogin
+    .create({
+      user_id: user._id,
+      login_datetime: Date(),
+      device_type: req.device.type,
+      ip_address: ip.address()
+    })
+    .then(profile => {
+      console.log(profile);
+      profile
+        .save()
+        .then(() => console.log("insert"))
+        .catch(err => console.error("insert error"));
+    })
+    .catch(err => console.log(err));
+
+  const token = user.generateAuthToken();
+
+  res.send(token);
+});
+
+router.post("/changepassword", auth, async (req, res) => {
+  const { error } = validate(req.params.password);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let users = await User.findOne({ _id: req.user._id });
+  if (!users)
+    return res.status(400).send("The user with the given ID was not found.");
+
+  const validPassword = req.body.password === users.password;
+  if (validPassword) return res.status(400).send("Using same password");
+
+  let user = await User.findByIdAndUpdate(req.user._id, {
+    password: req.body.password
+  })
+    .then(() => console.log("updated"))
+    .catch(err => console.error("update error"));
+  let updatedUser = await User.findOne({ _id: req.user._id });
+
+  res.status(200).send(updatedUser);
+});
+
+router.post("/forgotpassword", async (req, res) => {
+  const { error } = validate(req.params.password);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let users = await User.findOne({ email: req.body.email });
+  if (!users) return res.status(400).send("The given email was not found.");
+
+  const validPassword = req.body.password === users.password;
+  if (validPassword) return res.status(400).send("Using same password");
+
+  //   console.log(req.user.email);
+  let user = await User.findByIdAndUpdate(users._id, {
+    password: req.body.password
+  })
+    .then(() => console.log("updated"))
+    .catch(err => console.error("update error"));
+  let updatedUser = await User.findOne({ email: req.body.email });
+
+  res.status(200).send(updatedUser);
+});
+
+/* GET LOGOUT */
+router.get("/logout", function(req, res) {
+  // req.session.destroy();
+  console.log(req.session);
+  res.send("logout success!");
+});
+
+router.get("/get", async (req, res) => {
+  const customers = await User.find();
+  res.send(customers);
+});
+
+module.exports = router;
